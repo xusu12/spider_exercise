c8e72c3ee114f0f55caac412c9f1f9e2fe5c5e1d
xusu <1104963431@qq.com>
2020-05-31 23:50:22 +0800

    添加sycm js

---
+++ sycm_js/test.js
@@ -0,0 +1,144 @@
+var f_a_init = function(e, t) {
+                    e = this.words = e || [],
+                    this.sigBytes = null != t ? t : 4 * e.length
+                };
+
+var f_u_stringify = function(e) {
+                for (var t = e.words, n = e.sigBytes, r = [], o = 0; o < n; o++) {
+                    var i = t[o >>> 2] >>> 24 - o % 4 * 8 & 255;
+                    r.push(String.fromCharCode(i))
+                }
+                return r.join("")
+            };
+
+var f_u_parse = function(e) {
+                for (var t = e.length, n = [], r = 0; r < t; r++)
+                    n[r >>> 2] |= (255 & e.charCodeAt(r)) << 24 - r % 4 * 8;
+                return new f_a_init(n,t)
+            };
+var f_Utf8 = {
+    stringify: function (e) {
+        try {
+            return decodeURIComponent(escape(f_u_stringify(e)))
+        } catch (e) {
+            throw new Error("Malformed UTF-8 data")
+        }
+    },
+    parse: function (e) {
+        return f_u_parse(unescape(encodeURIComponent(e)))
+    }
+};
+
+var f_Hex = {
+    stringify: function(e) {
+        for (var t = e.words, n = e.sigBytes, r = [], o = 0; o < n; o++) {
+            var i = t[o >>> 2] >>> 24 - o % 4 * 8 & 255;
+            r.push((i >>> 4).toString(16)),
+            r.push((15 & i).toString(16))
+        }
+        return r.join("")
+    },
+    parse: function(e) {
+        for (var t = e.length, n = [], r = 0; r < t; r += 2)
+            n[r >>> 3] |= parseInt(e.substr(r, 2), 16) << 24 - r % 8 * 4;
+        return new f_a_init(n,t / 2)
+    }
+};
+
+// var f_hex_parse = function(e) {
+//                 for (var t = e.length, n = [], r = 0; r < t; r += 2)
+//                     n[r >>> 3] |= parseInt(e.substr(r, 2), 16) << 24 - r % 8 * 4;
+//                 return new f_a_init(n,t / 2)
+//             };
+
+var f_base64_stringify = function(e) {
+            var t = e.words
+              , n = e.sigBytes
+              , r = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+            // e.clamp();
+            for (var o = [], i = 0; i < n; i += 3)
+                for (var a = (t[i >>> 2] >>> 24 - i % 4 * 8 & 255) << 16 | (t[i + 1 >>> 2] >>> 24 - (i + 1) % 4 * 8 & 255) << 8 | t[i + 2 >>> 2] >>> 24 - (i + 2) % 4 * 8 & 255, c = 0; c < 4 && i + .75 * c < n; c++)
+                    o.push(r.charAt(a >>> 6 * (3 - c) & 63));
+            var s = r.charAt(64);
+            if (s)
+                for (; o.length % 4; )
+                    o.push(s);
+            return o.join("")
+        };
+
+var f_Pkcs7 = {
+    pad: function(e, t) {
+        for (var n = 4 * t, r = n - e.sigBytes % n, o = r << 24 | r << 16 | r << 8 | r, i = [], a = 0; a < r; a += 4)
+            i.push(o);
+        var s = c.create(i, r);
+        e.concat(s)
+    },
+    unpad: function(e) {
+        var t = 255 & e.words[e.sigBytes - 1 >>> 2];
+        e.sigBytes -= t
+    }
+};
+
+// var f_CBC = function() {
+//         var e = h.extend();
+//         function t(e, t, n) {
+//             var r = this._iv;
+//             if (r) {
+//                 var o = r;
+//                 this._iv = void 0
+//             } else
+//                 o = this._prevBlock;
+//             for (var i = 0; i < n; i++)
+//                 e[t + i] ^= o[i]
+//         }
+//         return e.Encryptor = e.extend({
+//             processBlock: function(e, n) {
+//                 var r = this._cipher
+//                   , o = r.blockSize;
+//                 t.call(this, e, n, o),
+//                 r.encryptBlock(e, n),
+//                 this._prevBlock = e.slice(n, n + o)
+//             }
+//         }),
+//         e.Decryptor = e.extend({
+//             processBlock: function(e, n) {
+//                 var r = this._cipher
+//                   , o = r.blockSize
+//                   , i = e.slice(n, n + o);
+//                 r.decryptBlock(e, n),
+//                 t.call(this, e, n, o),
+//                 this._prevBlock = i
+//             }
+//         }),
+//         e
+//     }();
+
+function main(e, t, n) {
+    "use strict";
+    // var r = encrypt_r;
+    //   , o = n(1008)
+    //   , i = n(1009)
+    //   , a = n(7)
+    //   , c = new o.default;
+    // c.setPublicKey(i.default);
+
+    var s = "w28Cz694s63kBYk4"
+      , u = f_Utf8.parse(s)
+      , l = {
+        iv: f_Utf8.parse("4kYBk36s496zC82w"),
+        // mode: f_CBC,
+        padding: f_Pkcs7
+    };
+    var data1 = f_base64_stringify(f_Hex.parse(e));
+    var data2 = r.AES.decrypt(data1, u, l);
+    n = JSON.parse(f_Utf8.stringify(data2));
+
+    // n = JSON.parse(r.AES.decrypt(function(e) {
+    //                 return f_base64_stringify(f_Hex.parse(e))
+    //             }(e), u, l).toString(f_Utf8));
+
+    console.log(n)
+}
+
+data = 'd4db6d83fcca89e3152182ce2cd197de859b03025a7ab4821bb9c9f4057ea5320d9b4b9356ffe28d0b4c9cdabdc52449cbc3a1396cc9e7b09af6abd547aed89df5a0d5ffbcbaddc95a9938efbd6a45f10f6357f9f821a76e9f9397bf0391ad15c9a8e19db60132b627ce3bd694588393ac706af15e132c35bda46d594dc4ed3a426b927c55bbfa68ecf2d417093a741a513f1a052efadeecf6d6f528410ab05c47bd5cc967d7756285e5c5a89480665a6c3c1798d7675f318c4bf2ae4f5590a4d3ebbbc64e5fbf26ded9683622e5dcec7254292039624bc23a46e715f844e924993fc13236f0c3428281d801753d96c88ef4eede001c89e4199c462311442b4afa26b69bce950e5e1f68923c71348a33f3f49b2a89dd1c3f208ed2a2aa3684b5d861b06385e09342b21e21f6ffbaac69526cd8d5abc5b7449fd6548b879e155e5381118445480ed97e92b39d61ad68361d9b0f3a818c8623fc8cb80f80e2f4831d391d11ebe3abb41158c21adfcb3088a1042b5ce03732f8f62605836ed227ad56f69e174f35cd71a9389441e13f330ac2d697b6bcf5e27d0cf1279346a35694b49d951afc19f31b7ca2b02c17f20041b8fa1568d42a300bf050590434e5d8688139bee0287f9b8ee0a671496b49da95a579a3cb30688a2ae0d1fc6905428d316ddae29703703bea318206cda0354940488a4c6e292090bdd16db04ea6bdcd4c8b67233075147b6d8d711df4373822e6ffe77849ffa55f6e7c2ddc4a22ef934ac2739288f346e14d3ed320981d2330c62fbe4292650108ee2c1a1fe2d44904a8'
+main(data, '', '');
