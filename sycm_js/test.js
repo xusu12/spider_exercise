4a156b53e3ac11dfd308f5e14815ff86c7edb4d6
xusu <1104963431@qq.com>
2020-06-02 23:14:10 +0800

    添加sycm js

--- sycm_js/test.js
+++ sycm_js/test.js
@@ -113,15 +113,41 @@ var f_Pkcs7 = {
 //         e
 //     }();
 
-var f_decrypt = function(e, t, n, r) {
-            return r = this.cfg.extend(r),
-            t = this._parse(t, r.format),
-            e.createDecryptor(n, r).finalize(t.ciphertext)
-        };
+// var f_decrypt = function(e, t, n, r) {
+//             return r = this.cfg.extend(r),
+//             t = this._parse(t, r.format),
+//             e.createDecryptor(n, r).finalize(t.ciphertext)
+//         };
+//
+// var f_aes_decrypt = function(n, r, o) {
+//                         return f_decrypt(t, n, r, o)
+//                     };
 
-var f_aes_decrypt = function(n, r, o) {
-                        return f_decrypt(t, n, r, o)
-                    };
+var f_parse = function(e) {
+            var t = e.length
+              , n = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+              , r = this._reverseMap;
+            if (!r) {
+                r = this._reverseMap = [];
+                for (var i = 0; i < n.length; i++)
+                    r[n.charCodeAt(i)] = i
+            }
+            var a = n.charAt(64);
+            if (a) {
+                var c = e.indexOf(a);
+                -1 !== c && (t = c)
+            }
+            return function(e, t, n) {
+                for (var r = [], i = 0, a = 0; a < t; a++)
+                    if (a % 4) {
+                        var c = n[e.charCodeAt(a - 1)] << a % 4 * 2
+                          , s = n[e.charCodeAt(a)] >>> 6 - a % 4 * 2;
+                        r[i >>> 2] |= (c | s) << 24 - i % 4 * 8,
+                        i++
+                    }
+                return o.create(r, i)
+            }(e, t, r)
+        };
 
 function main(e, t, n) {
     "use strict";
@@ -140,14 +166,15 @@ function main(e, t, n) {
             padding: f_Pkcs7
         };
     var data1 = f_base64_stringify(f_Hex.parse(e));
-    var data2 = f_aes_decrypt(data1, u, l);
-    n = JSON.parse(f_Utf8.stringify(data2));
+    // var data2 = f_aes_decrypt(data1, u, l);
+    // n = JSON.parse(f_Utf8.stringify(data2));
 
     // n = JSON.parse(r.AES.decrypt(function(e) {
     //                 return f_base64_stringify(f_Hex.parse(e))
     //             }(e), u, l).toString(f_Utf8));
+    var data2 = f_parse(data1);
 
-    console.log(n)
+    console.log(data1)
 }
 
 data = 'd4db6d83fcca89e3152182ce2cd197de859b03025a7ab4821bb9c9f4057ea5320d9b4b9356ffe28d0b4c9cdabdc52449cbc3a1396cc9e7b09af6abd547aed89df5a0d5ffbcbaddc95a9938efbd6a45f10f6357f9f821a76e9f9397bf0391ad15c9a8e19db60132b627ce3bd694588393ac706af15e132c35bda46d594dc4ed3a426b927c55bbfa68ecf2d417093a741a513f1a052efadeecf6d6f528410ab05c47bd5cc967d7756285e5c5a89480665a6c3c1798d7675f318c4bf2ae4f5590a4d3ebbbc64e5fbf26ded9683622e5dcec7254292039624bc23a46e715f844e924993fc13236f0c3428281d801753d96c88ef4eede001c89e4199c462311442b4afa26b69bce950e5e1f68923c71348a33f3f49b2a89dd1c3f208ed2a2aa3684b5d861b06385e09342b21e21f6ffbaac69526cd8d5abc5b7449fd6548b879e155e5381118445480ed97e92b39d61ad68361d9b0f3a818c8623fc8cb80f80e2f4831d391d11ebe3abb41158c21adfcb3088a1042b5ce03732f8f62605836ed227ad56f69e174f35cd71a9389441e13f330ac2d697b6bcf5e27d0cf1279346a35694b49d951afc19f31b7ca2b02c17f20041b8fa1568d42a300bf050590434e5d8688139bee0287f9b8ee0a671496b49da95a579a3cb30688a2ae0d1fc6905428d316ddae29703703bea318206cda0354940488a4c6e292090bdd16db04ea6bdcd4c8b67233075147b6d8d711df4373822e6ffe77849ffa55f6e7c2ddc4a22ef934ac2739288f346e14d3ed320981d2330c62fbe4292650108ee2c1a1fe2d44904a8'
